af1fe335890fa5ad1dc78c2cf34ab6f39005a5e2
h4fide <40579109+h4fide@users.noreply.github.com>
2022-05-05 10:00:17 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,48 +1,56 @@
 import requests
 
-PROVIDER_URL = "http://bam.magnetomedia.de/trackinghistory.php"
-lastOBool = 'true'
+PROVIDER = "http://bam.magnetomedia.de/trackinghistory.php"
+LOBool = 'true'
 
-class TrackerApi(object):
+class Tracker(object):
     """ 
     It's a class that makes a POST request to a URL, and returns the JSON response
     """
     @staticmethod
-    def getPackageInformation(trackingNumber: str):
+    def PackageInfo(trackingNumber: str):
         """
         It takes a tracking number, and returns a dictionary of information about the package
         
         :param trackingNumber: The tracking number of the package
         :return: A dictionary of the package information.
         """
-        req = requests.get(PROVIDER_URL + '?lastOperation='+ lastOBool + '&trackingNumber=' + trackingNumber)
+        try:
+            req = requests.get(f'{PROVIDER}?lastOperation={LOBool}&trackingNumber={trackingNumber}' ,timeout=5.0)
+        except requests.exceptions.Timeout:
+            print('Timeout')
+        except requests.exceptions.RequestException as er:
+            print('OOps Somthing Wrong')
+            raise SystemExit(er)
 
-        if req.json()["response"] != '0':
-            raise ValueError("Unknown Error!")
-        else:
+        if req.status_code == 200:
             return req.json()["data"][0]
+        else:
+            SystemExit('Http Error')
 
 
     @staticmethod
-    def getLastStatut(trackingNumber: str) -> None:
+    def LastStatus(trackingNumber: str) -> None:
         """
         It returns the last status of the package
         
         :param trackingNumber: The tracking number of the package
         :return: The last status of the package
         """
-        info = TrackerApi.getPackageInformation(trackingNumber)
-        
-        if lastOBool == 'false':
-            etat = info['etat']
-            if etat == 'Infos.Indisponibles':
-                return 'Missing' # This Package Is Not Registerd In The System Yet Or Tracking Code is Wrong
-            return etat
-        if lastOBool == 'true':
-            dernier_statut = info["dernier_statut"]
-            if dernier_statut == 'Neant':
-                return 'Missing' # This Package Is Not Registerd In The System Yet 
-            return dernier_statut
-        else:
-            return "Unknown"
+        info = Tracker.PackageInfo(trackingNumber)
+        try:
+            if LOBool == 'false':
+                etat = info['etat']
+                if etat == 'Infos.Indisponibles':
+                    return 'Missing'
+                return etat
+            if LOBool == 'true':
+                last_status = info["dernier_statut"]
+                if last_status == 'Neant':
+                    return 'Missing' # This Package Is Not Registerd In The System Yet or Tracking Code is Wrong
+                return last_status
+            else:
+                return "Unknown"
+        except:
+            print('No response')
     
